59e8e082ad2db46fc5a627b291338d99adfd69da
AmritaKB <bains.amrita.k@gmail.com>
2020-03-03 15:29:59 +0000

    Attempt2
    Signed-off-by: AmritaKB <bains.amrita.k@gmail.com>

--- index.php
+++ index.php
@@ -11,11 +11,11 @@ and open the template in the editor.
     </head>
     <body>
         <h1>Welcome to a library of Yorkshire Films! </h1>
-
+        <h2> This is brought to you by The Yorkshire Puddings </h2>
+        <h3> 140g plain flour, 4 eggs, 200ml milk and 5 ambitous coders </h3>
 
         <h2> Lily was here....</h2>
         <h2> So was Claire...</h2>
-        <h2> Tese returned to test pulling...</h2>
        
         <p style="text-align:center;"> Yorkshire. </p>
 
@@ -24,7 +24,8 @@ and open the template in the editor.
         <p style="text-align:center;"> You’d think we could rest on our laurels after that, but we kept on pushing boundaries like an old man rolling down a hill in a tin bath. </p>
 
         <p style="text-align:center;"> Here you’ll find some of examples of the talent and beauty to be found in this amazing region. The films available to borrow from this library have all been filmed or based in Yorkshire. Some are gritty and dark, some wild and romantic, some shocking and funny, but all are great examples of why Yorkshire is brilliant.</p>
-          
+        
+         <h3> Amrita showed up and scratched her head </h3> 
         <?php        ?>
     </body>
 </html>
