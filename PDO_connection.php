17a5b181de3f6538af847752f9ebc8dc4c9fc72b
Claire Winterbottom <clairemwinterbottom@gmail.com>
2020-03-22 22:15:32 +0000

    Connection correction

--- PDO_connection.php
+++ PDO_connection.php
@@ -3,19 +3,14 @@
 // It would be loaded into every page that uses an autoloader, so we wouldn't have to include this page also.
 // However, as we're using two different types of connection, this would cause a conflict with Gabby's current work.
 
-//local server information
-$pdoHost = "localhost"; //Servername
-$pdoUser = "root"; //Username for the server
-$pdoPassword = ""; //Password for the server
-$pdoDB = "YorkshireFilms"; //Your database name
-
-try {
-      $pdoConnect = new PDO("mysql:host=$pdoHost;dbname:$pdoDB",$pdoUser,$pdoPassword);
-      
-      $pdoConnect ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
- 
-      
-} catch (PDOException $e) {
-	echo $e.getMessage();
-}
-              
+    const DB_DSN = 'mysql:host=localhost;dbname=Yorkshire-Films';
+    const DB_USER = 'root';
+    const DB_PASS = '';
+    
+    try {
+        $pdo = new PDO(DB_DSN, DB_USER, DB_PASS);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        die($e->getMessage());
+    }
+    
