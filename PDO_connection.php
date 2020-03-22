3ea55f5857df2b414d6b25b7b86ef209c8480049
Claire Winterbottom <clairemwinterbottom@gmail.com>
2020-03-22 11:55:48 +0000

    Note added to PDO_connection.php re the implementation of the DB link.

--- PDO_connection.php
+++ PDO_connection.php
@@ -1,4 +1,8 @@
 <?php
+// A better method for implentation of this could be to have this file as a class rather than a standard PHP doc. 
+// It would be loaded into every page that uses an autoloader, so we wouldn't have to include this page also.
+// However, as we're using two different types of connection, this would cause a conflict with Gabby's current work.
+
 //local server information
 $pdoHost = "localhost"; //Servername
 $pdoUser = "root"; //Username for the server
@@ -9,9 +13,9 @@ try {
       $pdoConnect = new PDO("mysql:host=$pdoHost;dbname:$pdoDB",$pdoUser,$pdoPassword);
       
       $pdoConnect ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      echo "connected to server";
+ 
       
 } catch (PDOException $e) {
-	die ($e.getMessage);
+	echo $e.getMessage();
 }
               
