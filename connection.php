af81e32e4ab3f3a396b75b760502cc76ce734813
Gabriele Virbasiute <g.virbasiute@gmail.com>
2020-03-23 15:25:20 +0000

    no message

--- connection.php
+++ connection.php
@@ -3,7 +3,7 @@
 $dbServername = "localhost"; //Servername
 $dbUsername = "root"; //Username for the server
 $dbPassword = ""; //Password for the server
-$dbName = "Yorkshire-Films"; //Your database name
+$dbName = "YorkshireFilms"; //Your database name
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName); //Needs to be in this order!
 
